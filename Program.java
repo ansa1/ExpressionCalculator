0be00e13ed6ada5da3d080af830d3f35f08677b4
petrNikolaev1 <petru4o144@gmail.com>
2018-09-09 14:28:56 +0300

    Expression Calculator cleanup

--- Program.java
+++ Program.java
@@ -1,11 +1,7 @@
-import java.io.*;
-import java.util.Scanner;
-
 public class Program {
 
-    public static final String inputFile = "in11.txt";
+    public static final String inputFile = "in.txt";
     public static final String outputFile = "out.txt";
-
 
     /*
      Launches the parser.
@@ -22,33 +18,4 @@ public class Program {
         writer.close();
     }
 
-    /**
-     * Converts the given json string to one line.
-     *
-     * @param json the given json string.
-     * @return obtained json string in one line.
-     */
-    public static String JSONtoLine(String json) {
-        return json.replace("\n", "").replace("\t", "");
-    }
-
-    /**
-     * Reads a line with an expression from the input file.
-     *
-     * @return the given line without spaces.
-     */
-    public static String readLine() {
-        String input = "";
-        File file = new File(inputFile);
-        try {
-            Scanner sc = new Scanner(file);
-            if (sc.hasNextLine()) {
-                input = sc.nextLine();
-            }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return input.replace(" ", "");
-    }
 }
